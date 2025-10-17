7241ae67c678147616ff34e5aa0492e83553e8a4
田心61 <2737504027@qq.com>
2025-10-17 11:52:34 +0800

    create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "LoginDialog.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    LoginDialog ld;
+    ld.show();
+    return a.exec();
+}
