da228c4effcbe69598c0ba8bce52c4fba8fc74d9
田心61 <2737504027@qq.com>
2025-10-17 11:56:44 +0800

    create SubjectInfo.cpp

---
+++ SubjectInfo.cpp
@@ -0,0 +1,34 @@
+#include "SubjectInfo.h"
+SubjectInfo* SubjectInfo::instance=nullptr;
+SubjectInfo *SubjectInfo::getInstance()
+{
+    if(instance==nullptr)
+    {
+        instance=new SubjectInfo;
+    }
+    return instance;
+}
+
+void SubjectInfo::releaseInstance()
+{
+    if(instance!=nullptr)
+    {
+        delete instance;
+        instance=nullptr;
+    }
+}
+
+SubjectInfo::SubjectInfo()
+{
+
+}
+
+SubjectInfo::SubjectInfo(const SubjectInfo *other)
+{
+    Q_UNUSED(other);
+}
+
+SubjectInfo::~SubjectInfo()
+{
+
+}
